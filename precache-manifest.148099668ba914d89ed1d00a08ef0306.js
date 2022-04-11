ddccb55df76dee09575b29536df0665b4c92f6b2
MauricioR998 <MauricioR998@users.noreply.github.com>
2022-04-11 05:12:01 +0000

    deploy: 1246aa2b63f69c44c7f96d588e0618dfd9e89687

---
+++ precache-manifest.148099668ba914d89ed1d00a08ef0306.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f574077364f72e9994a9a42cac5ab78",
+    "url": "https://MauricioR998.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "bc3c87c42fe411720ee2",
+    "url": "https://MauricioR998.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "af9d1fc7a935bc3aabc2",
+    "url": "https://MauricioR998.github.io/task-management-frontend/static/js/2.5ee4a3d8.chunk.js"
+  },
+  {
+    "revision": "bc3c87c42fe411720ee2",
+    "url": "https://MauricioR998.github.io/task-management-frontend/static/js/main.ec6b5b2e.chunk.js"
+  },
+  {
+    "revision": "61950a05e2a7b03235e6",
+    "url": "https://MauricioR998.github.io/task-management-frontend/static/js/runtime~main.85657888.js"
+  }
+]);
